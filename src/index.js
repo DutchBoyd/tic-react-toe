1bd6f04525d069f9bf0d3cc0b8c4382cd6f7a167
Dutch Boyd <dutch@dutchboyd.com>
2021-02-14 02:47:58 -0800

    Pulling square components into Board controller;

--- src/index.js
+++ src/index.js
@@ -16,19 +16,39 @@ class Square extends React.Component {
       return (
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return  (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)} 
+        />
+      );
     }
   
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     render() {
       const status = 'Next player: X';
       const footer = 'Copyright 2021 - Dutch Boyd';
