9d735af3b80077776701429aba47ad8825bb123c
Dutch Boyd <dutch@dutchboyd.com>
2021-02-14 03:27:21 -0800

    Adding calculateWinner function;

--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ function Square(props) {
     </button>
   );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
   
   class Board extends React.Component {
 
@@ -33,6 +53,9 @@ function Square(props) {
   
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -41,11 +64,19 @@ function Square(props) {
     }
 
     render() {
-      const status = 'Next Player\'s Move: ' + (this.state.xIsNext ? 'X' : 'O');
+      const title = 'Tic-React-Toe';
       const footer = 'Copyright 2021 - Dutch Boyd';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = winner + ' is the Winner!';
+      } else {
+        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+        
       return (
         <div>
+          <div className="title">{title}</div>
           <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
